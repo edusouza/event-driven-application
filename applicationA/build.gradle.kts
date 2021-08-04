74497e1ff5c7e45fe4ad5065c64d4a1096a5c128
Eduardo Olievira de Souza <souza.eduardo@gmail.com>
2021-08-04 16:45:06 -0300

    Upgrading Kotlin version and using Kotlin stdlib instead stdlib-jdk8

--- applicationA/build.gradle.kts
+++ applicationA/build.gradle.kts
@@ -3,8 +3,8 @@ import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
 plugins {
 	id("org.springframework.boot") version "2.4.9"
 	id("io.spring.dependency-management") version "1.0.11.RELEASE"
-	kotlin("jvm") version "1.4.32"
-	kotlin("plugin.spring") version "1.4.32"
+	kotlin("jvm") version "1.5.21"
+	kotlin("plugin.spring") version "1.5.21"
 }
 
 group = "br.com.eduardosouza"
@@ -25,7 +25,7 @@ dependencies {
 	implementation("com.fasterxml.jackson.module:jackson-module-kotlin")
 	implementation("com.google.cloud:spring-cloud-gcp-starter")
 	implementation("org.jetbrains.kotlin:kotlin-reflect")
-	implementation("org.jetbrains.kotlin:kotlin-stdlib-jdk8")
+	implementation("org.jetbrains.kotlin:kotlin-stdlib")
 	implementation("com.google.cloud:spring-cloud-gcp-data-datastore")
 	implementation("com.google.cloud:spring-cloud-gcp-starter-pubsub")
 	testImplementation("org.springframework.boot:spring-boot-starter-test")
